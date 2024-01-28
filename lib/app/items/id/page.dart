86ea7a3c20bf22ab8aef827f7012a35230889c02
wheatandcat <wheatandcat@gmail.com>
2024-01-28 12:53:30 +0900

    fix

--- lib/app/items/id/page.dart
+++ lib/app/items/id/page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:sampleflutter/components/appBar/common.dart';
 
@@ -20,64 +22,163 @@ class _ItemDetailState extends State<ItemDetail> {
         children: [
           Center(
               child: SizedBox(
-                  width: 300,
-                  height: 300,
+                  width: 250,
+                  height: 250,
                   child: Image.network(
                     'https://via.placeholder.com/150',
                     fit: BoxFit.cover,
                   ))),
           SizedBox(
+              height: 80,
               child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text("  0%",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(
-                width: 250,
-                child: Slider(
-                  activeColor: Colors.white,
-                  inactiveColor: Colors.black.withOpacity(0.2),
-                  value: _value,
-                  min: 0,
-                  max: 100,
-                  onChanged: (double value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
-                ),
-              ),
-              const Text("100%",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-            ],
-          )),
-          const Expanded(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text("  0%",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    width: 250,
+                    child: Slider(
+                      activeColor: Colors.white,
+                      inactiveColor: Colors.black.withOpacity(0.2),
+                      value: _value,
+                      min: 0,
+                      max: 100,
+                      onChanged: (double value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      },
+                    ),
+                  ),
+                  const Text("100%",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                ],
+              )),
+          const SizedBox(
+              width: 300,
+              height: 60,
               child: Center(
                   child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("ストック数 ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold)),
-              Flexible(
-                child: SizedBox(
-                    width: 100,
-                    height: 30,
-                    child: TextField(
-                        keyboardType: TextInputType.number,
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                        decoration: InputDecoration(labelText: ''))),
-              )
-            ],
-          ))),
+                children: <Widget>[
+                  SizedBox(
+                    width: 150,
+                    child: Text("ストック数",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Flexible(
+                      child: SizedBox(
+                    width: 120,
+                    height: 40,
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 0, top: 20),
+                        child: TextField(
+                            textAlign: TextAlign.center,
+                            cursorColor: Colors.white,
+                            keyboardType: TextInputType.number,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                            decoration: InputDecoration(
+                              labelText: '',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ))),
+                  ))
+                ],
+              ))),
+          const SizedBox(
+              width: 300,
+              height: 60,
+              child: Center(
+                  child: Row(
+                children: <Widget>[
+                  SizedBox(
+                      width: 150,
+                      child: Text("消費期限",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: SizedBox(
+                          child: Text("2021/12/31",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )))),
+                ],
+              ))),
+          const SizedBox(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                      width: 300,
+                      child: Text("MEMO",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold))),
+                  SizedBox(
+                      width: 300,
+                      height: 100,
+                      child: Padding(
+                          padding: EdgeInsets.only(top: 0),
+                          child: TextField(
+                              maxLines: null, // 複数行入力可能
+                              minLines: 3,
+                              cursorColor: Colors.white,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.black26,
+                                border: InputBorder.none,
+                              )))),
+                ],
+              )),
+          Center(
+              child: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: SizedBox(
+                width: 150,
+                height: 40,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        color: Colors.white,
+                        width: 3,
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50) //こちらを適用
+                          ),
+                      backgroundColor: Colors.transparent),
+                  onPressed: () => {debugPrint("保存する")},
+                  child: const Text("保存する",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                )),
+          ))
         ],
       ),
     );
