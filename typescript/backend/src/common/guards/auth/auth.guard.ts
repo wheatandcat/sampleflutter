3e70573e7b52d329409e507d5a5ad499fb108820
wheatandcat <wheatandcat@gmail.com>
2024-08-03 23:11:09 +0900

    add: guest

--- typescript/backend/src/common/guards/auth/auth.guard.ts
+++ typescript/backend/src/common/guards/auth/auth.guard.ts
@@ -1,10 +1,15 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import {
+  type CanActivate,
+  type ExecutionContext,
+  Injectable,
+} from '@nestjs/common'
 import { GqlExecutionContext } from '@nestjs/graphql'
 import { PrismaService } from '@src/modules/prisma/prisma.service'
 import admin from 'firebase-admin'
-import { type User } from '@prisma/client'
+import type { User } from '@prisma/client'
 
 export type Auth = {
+  guest: boolean
   uid: string
   userId: number
   user: User
@@ -18,24 +23,57 @@ export class AuthGuard implements CanActivate {
     const ctx = GqlExecutionContext.create(context)
     const request = ctx.getContext().req
 
-    const token = request.headers['authorization']
-    const result = await admin.auth().verifyIdToken(token)
+    const token = request.headers.authorization
+    const guestUid = request.headers.guest
+    if (token) {
+      const result = await admin.auth().verifyIdToken(token)
 
-    const user = await this.prisma.user.findFirst({
-      where: {
-        uid: result.uid,
-      },
-    })
+      const user = await this.prisma.user.findFirst({
+        where: {
+          uid: result.uid,
+        },
+      })
 
-    if (!user) {
-      throw new Error('User not found')
+      if (!user) {
+        throw new Error('User not found')
+      }
+
+      request.auth = {
+        guest: false,
+        uid: user.uid,
+        userId: user.id,
+        user: user,
+      } as Auth
+    } else if (guestUid) {
+      const guest = await this.prisma.guest.findFirst({
+        where: {
+          uid: guestUid,
+        },
+      })
+
+      if (!guest) {
+        throw new Error('Guest not found')
+      }
+
+      const user = await this.prisma.user.findFirst({
+        where: {
+          id: guest.userId,
+        },
+      })
+
+      if (!user) {
+        throw new Error('User not found')
+      }
+
+      request.auth = {
+        guest: true,
+        uid: user.uid,
+        userId: user.id,
+        user: user,
+      } as Auth
+    } else {
+      throw new Error('Unauthorized')
     }
-
-    request.auth = {
-      uid: result.uid,
-      userId: user.id,
-      user: user,
-    } as Auth
 
     return true
   }
