04013d7132f09f54bc99c5aea790f5b0966f9da8
Yohei Iino <wheatandcat@gmail.com>
2024-11-03 13:37:38 +0900

    add: graphql document (#85)

---
+++ typescript/backend/docs/_app/immutable/entry/start.DqCEd9Xf.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.x0XV_q-j.js";export{t as start};
---
+++ typescript/backend/magidoc.mjs
@@ -0,0 +1,21 @@
+export default {
+  introspection: {
+    type: 'sdl', // スキーマをファイルから読み込む場合は "sdl" を指定する
+    paths: ['src/schema.graphql'], // スキーマファイルのパスを指定する
+  },
+  website: {
+    // テンプレートを指定する
+    // v6.1.0 時点でデフォルトで用意されてるのは "carbon-multi-page" のみ
+    template: 'carbon-multi-page',
+
+    // Web サイトのルートパスを指定する
+    // Public な GitHub Pages の URL は https://<オーナー名>.github.io/<リポジトリ名>/ のようになるので、
+    // ここには "/<リポジトリ名>" を指定する必要がある
+    siteRoot: '/example-repository',
+    options: {
+      queryGenerationFactories: {
+        Time: '2023-11-03T10:15:30Z', // 例としてISO 8601形式の日時
+      },
+    },
+  },
+}
